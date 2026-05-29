d751e531265207a6a719bb0c876e79eed4098039
Ben Kwon <arkanumbk@gmail.com>
2026-05-29 11:12:06 -0700

    Implemented Inputted Port validation

--- index.js
+++ index.js
@@ -2,12 +2,34 @@
 
 import { program } from 'commander';
 
-function parsePort(port){
-  return `From parsePort function ` + port
+function validatePort(port){// response: {isValid, errorMessage, Validated-Port-Number(null otherwise)}
+  const number = Number(port);
+
+  if(!Number.isInteger(number)) return {// Check if entered port number is a whole number
+    isValid : false,
+    errorMessage : `Entered Port: ${number} is not a whole number`,
+    port : null
+  }
+
+  if(number < 1 || number > 65535) return {
+    isValid : false,
+    errorMessage : `Port number must be between 1 and 65535. Current input: ${number}`,
+    port : null
+  }
+
+  return {
+    isValid : true,
+    errorMessage: null,
+    port : number
+  }
 }
 
-function parseURL(url){
-  return 'From parseURL function ' + url
+function validateURL(url){
+  return {
+    isValid : true,
+    errorMessage: null,
+    validatedUrl : url
+  }
 }
 
 program
@@ -16,21 +38,31 @@ program
   .version('0.0.1');
 
 program
-  .option('-p, --port <number>', 'port on which the caching proxy server will run', parsePort)
-  .option('-o, --origin <url>', 'URL of the server to which the requests will be forwarded to', parseURL)
+  .option('-p, --port <number>', 'port on which the caching proxy server will run')
+  .option('-o, --origin <url>', 'URL of the server to which the requests will be forwarded to')
   .option('-c, --clear-cache', 'Clear the cache')
   .action((options) =>{
+    let port = options.port;
+    let url = options.origin;
+
     if(options.clearCache){
       console.log('clear cache requested')
       return;
     }
 
-    if( !options.port || !options.origin ){
+    if( !port || !url ){
       program.error("Missing required options: --port and --origin are required unless --clear-cache is used")
     }
 
-    console.log(options.port)
-    console.log(options.origin)
+    let {isValid, errorMessage, validatedPort} = validatePort(options.port)
+    if(!isValid) program.error(errorMessage);
+
+    let validatedURL = "";
+    ({isValid, errorMessage, validatedURL} = validateURL(options.url))
+    if(!isValid) program.error(errorMessage);
+
+    console.log(validatedPort)
+    console.log(validatedURL)
   })
 
 program.parse();
